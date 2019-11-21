49090798504cdd3371516b4409659d786fd4749f
Elizabeth Ernst <eliernst@gmail.com>
2019-11-21 12:16:19 -0600

    finished studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,65 @@
 package test;
 
+import main.BalancedBrackets;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
+    BalancedBrackets test_Class;
+@Before
+public void launchTest(){
+      BalancedBrackets test_Class = new BalancedBrackets();
+}
 
     //TODO: add tests here
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(test_Class.hasBalancedBrackets("[]"));
+    }
     @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
-
-
+    @Test
+    public  void launchBracket(){
+        assertTrue(test_Class.hasBalancedBrackets("[LaunchCode]"));
+    }
+    @Test
+    public void codeBracket(){
+        assertTrue(test_Class.hasBalancedBrackets("Launch[Code]"));
+    }
+    @Test
+    public void frontBracket(){
+        assertTrue(test_Class.hasBalancedBrackets("[]LaunchCode"));
+    }
+    @Test
+    public void noBracket(){
+        assertTrue(test_Class.hasBalancedBrackets(""));
+    }
+    @Test
+    public void emptyBracket(){
+        assertTrue(test_Class.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void noEndBracket(){
+        assertFalse(test_Class.hasBalancedBrackets("["));
+    }
+    @Test
+    public void noBeginBracket(){
+        assertFalse(test_Class.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void tooManyBeginBracket(){
+        assertFalse(test_Class.hasBalancedBrackets("[[["));
+    }
+    @Test
+    public void tooManyEndBracket(){
+        assertFalse(test_Class.hasBalancedBrackets("]]]"));
+    }
+    @Test
+    public void wayTooManyEndBracket(){
+        assertFalse(test_Class.hasBalancedBrackets("]]]]]"));
+    }
 }
